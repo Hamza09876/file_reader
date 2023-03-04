70ec4bfd1f33b72ebc605bf4a89c274785353c51
Hamza09876 <hamzasajid4040@gmail.com>
2023-03-04 13:14:32 +0500

    init reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//import file system library
+use std::fs;
+//function to read file info.txt
 fn main() {
-    println!("Hello, world!");
+    let file_contents = fs::read_to_string("info.txt")
+        .expect("Should have been able to read the file");
+    println!("info.txt context are {file_contents}");
 }
