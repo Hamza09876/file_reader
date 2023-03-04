721df9ea2cc5e873c3fa6a8cd87ba72d7d29a6f3
Hamza09876 <hamzasajid4040@gmail.com>
2023-03-04 14:47:57 +0500

    parsing dynamic json

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use serde_json::Value;
+use serde_json::{Number, Value};
 use std::fs;
 fn main() {
-    let sales_and_products = {
+    let mut sales_and_products = {
         let file_content = fs::read_to_string("./data/sales.json").expect("LogRocket: error reading file");
         serde_json::from_str::<Value>(&file_content).expect("LogRocket: error serializing to JSON")
     };
     println!("{:?}", serde_json::to_string_pretty(&sales_and_products).expect("LogRocket: error parsing to JSON"));
+
+    if let Value::Number(quantity) = &sales_and_products["sales"][1]["quantity"] {
+        sales_and_products["sales"][1]["quantity"] =
+            Value::Number(Number::from_f64(quantity.as_f64().unwrap() + 3.5).unwrap());
+    }
+    fs::write(
+        "./data/sales.json",
+        serde_json::to_string_pretty(&sales_and_products).expect("LogRocket: error parsing to JSON"),
+    )
+    .expect("LogRocket: error writing to file");
 }
